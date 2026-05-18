ae733a679d0bbcbfa9359640d553bc59709138ff
aaaaaaaa222a <jangh3073@gmail.com>
2026-05-19 01:26:06 +0900

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,10 +1,10 @@
 // Supabase project settings
 // Supabase Dashboard > Project Settings > API 에서 아래 두 값을 복사해서 붙여넣으세요.
 window.SUPABASE_URL = 'https://okrcliobkqcnyihwsmfo.supabase.co';
-window.SUPABASE_ANON_KEY = 'YOUR-SUPABASE-ANON-KEY';
+window.SUPABASE_ANON_KEY = 'sb_publishable_W7CWEuYnV06Z6gSTLNBCFQ_CMc1ELuJ';
 
 // 글을 작성할 관리자 이메일입니다.
-window.STORY_ADMIN_EMAIL = 'sb_publishable_W7CWEuYnV06Z6gSTLNBCFQ_CMc1ELuJ';
+window.STORY_ADMIN_EMAIL = 'janghayung@naver.com';
 
 // 사이트 로그인 팝업에서 입력할 관리자 아이디입니다.
 window.STORY_ADMIN_ID = 'vbxj';
